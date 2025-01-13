9a9078ea3af3123c38e0154c45a335548c574b12
lenne sebastien <sebastienlenne@gmail.com>
2025-01-13 13:52:48 +0100

    Update Next.js configuration and package dependencies
    - Added 'localhost' and 'res.cloudinary.com' to the image domains in next.config.js for improved image handling.
    - Updated remote patterns to allow image loading from localhost and adjusted Cloudinary path for broader access.
    - Introduced new dependency '@radix-ui/react-alert-dialog' version 1.1.4 in package.json and package-lock.json.
    - Upgraded '@radix-ui/react-tabs' to version 1.1.2 and other related Radix UI components to their latest versions for enhanced functionality and stability.
    
    These changes enhance image management capabilities and ensure the project uses the latest UI components.

--- next.config.js
+++ next.config.js
@@ -1,14 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
+    domains: [
+      'localhost',
+      'res.cloudinary.com'  // Pour les images Cloudinary
+    ],
     remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '3000',
+        pathname: '/images/**',
+      },
       {
         protocol: 'https',
         hostname: 'res.cloudinary.com',
-        pathname: '/dp8cn1dj2/**',
+        pathname: '/**',
       },
     ],
-    domains: ['res.cloudinary.com', 'example.com'],
   },
   webpack: (config, { dev, isServer }) => {
     if (!dev) {
